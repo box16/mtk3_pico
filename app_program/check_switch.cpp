575437c86c57a9692326276e1e8aa81bf51f00cb
box16 <inoinobox16@gmail.com>
2025-03-03 20:48:23 +0900

    スイッチチェックのひな形作成

---
+++ app_program/check_switch.cpp
@@ -0,0 +1,8 @@
+#include <tk/tkernel.h>
+#include <bsp/libbsp.h>
+#include "check_switch.h"
+
+void check_switch(INT stacd, void *exinf)
+{
+
+}
---
+++ app_program/check_switch.h
@@ -0,0 +1,8 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	void check_switch(INT stacd, void *exinf);
+#ifdef __cplusplus
+}
+#endif
