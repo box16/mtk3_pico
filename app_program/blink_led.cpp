0392fc37d83ca3f36c5bba5a7bd041348d9f1d07
box16 <inoinobox16@gmail.com>
2025-03-29 20:36:42 +0900

    blink_ledに初期化と終了を追加

--- app_program/blink_led.cpp
+++ app_program/blink_led.cpp
@@ -3,7 +3,8 @@
 #include "tasks.h"
 #include "event_flag.h"
 
-UW rand(UW& state) {
+UW rand(UW& state) 
+{
     // Using XorShift 32bit
     state ^= state << 13;
     state ^= state >> 17;
@@ -11,39 +12,51 @@ UW rand(UW& state) {
     return state;
 }
 
-void blink_leds(INT stacd, void *exinf)
-{   
-    static constexpr UB PIN_NUM = 5;
-    static constexpr UB led_pins[PIN_NUM] = {10,12,14,19,21};
-    for(UB i=0; i<PIN_NUM; i++)
+static BOOL has_been_initialized = FALSE;
+void init(const UB num,
+          const UB* pins)
+{
+    for(UB i=0; i<num; i++)
     {
-        gpio_set_pin(led_pins[i], GPIO_MODE_OUT);
+        gpio_set_pin(pins[i], GPIO_MODE_OUT);
     }
     // On board LED
     gpio_set_pin(25, GPIO_MODE_OUT);
+
+    has_been_initialized = TRUE;
+}
+
+void blink_leds(INT stacd, void *exinf)
+{
+    static constexpr UB PIN_NUM = 5;
+    static constexpr UB led_pins[PIN_NUM] = {10,12,14,19,21};
+    if(!has_been_initialized){
+        init(PIN_NUM,led_pins); 
+    }
 
     static constexpr UB NODES_MAX = 99;
     static UB nodes[NODES_MAX] = {};
     static UB now_node_num = 0;
     static UW seed = 256;
-    while (TRUE)
-    {
-        if(now_node_num >= NODES_MAX) break;
 
-        UB next = rand(seed) % PIN_NUM;
-        nodes[now_node_num] = next;
-        now_node_num++;
-        gpio_set_val(25, 1);
+    if(now_node_num >= NODES_MAX){
+        tk_ext_tsk();
+    }
+
+    UB next = rand(seed) % PIN_NUM;
+    nodes[now_node_num] = next;
+    now_node_num++;
+    gpio_set_val(25, 1);
+    tk_dly_tsk(500);
+    for(UB i=0; i<now_node_num; i++)
+    {
+        UB index = nodes[i];
+        gpio_set_val(led_pins[index], 1);
         tk_dly_tsk(500);
-        for(UB i=0; i<now_node_num; i++)
-        {
-            UB index = nodes[i];
-            gpio_set_val(led_pins[index], 1);
-            tk_dly_tsk(500);
-            gpio_set_val(led_pins[index], 0);
-            tk_dly_tsk(500);
-        }
-        gpio_set_val(25, 0);
-        tk_dly_tsk(1000);
-    }   
+        gpio_set_val(led_pins[index], 0);
+        tk_dly_tsk(500);
+    }
+    gpio_set_val(25, 0);
+    
+    tk_ext_tsk();
 }
